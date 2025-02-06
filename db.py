35bdc85750a7fd83847333c5e2a664aedbc6934b
tesserxt <tesserxt@users.noreply.github.com>
2025-02-06 15:40:00 +0530

    Create DataBase that saves ytContent and filter Comments out of it
    Without this we've to keep fetching json data from youtube api.

--- db.py
+++ db.py
@@ -1,8 +1,32 @@
 import json
+import os
 
 class Database:
 
     def save_data(self, data):
-        with open("ytCommentData.json", "w") as f:
+        with open("ytCommentsData.json", "w") as f:
             json.dump(data, f, indent=4)
 
+    def get_filter_data(self): #To get comments only
+        try:
+            with open("ytComments.json", "r") as fcmnts: 
+                comments = json.load(fcmnts)
+                return comments
+        
+        except:
+            if os.path.exists("ytCommentsData.json"):
+                with open("ytCommentsData.json", "r") as f:
+                    ytContent = json.load(f)
+                    print(len(ytContent["items"]))
+                    comments = { 
+                        "inputs": [
+                            item["snippet"]["topLevelComment"]["snippet"]["textOriginal"] 
+                            for item in ytContent["items"]
+                        ]
+                    }
+                    return comments
+
+            else:
+                print("Error: Cannot filter ytCommentsData.")
+                return -1
+        
