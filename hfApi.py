1d460bfe71be31949653a55be7c17e873abe7501
tesserxt <tesserxt@users.noreply.github.com>
2025-02-06 21:01:23 +0530

    Add 'existing' param to get previously analyzed data

--- hfApi.py
+++ hfApi.py
@@ -15,12 +15,25 @@ class HuggingFaceInferenceApi:
     def get_api_key(self):
         return self.__api_key
 
-    def run_analysis(self):
-        API_URL = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment-latest"
-        headers = {"Authorization": f"Bearer {self.get_api_key()}"}
-        d = Database()
-        payload = d.get_filter_data()
-        response = requests.post(API_URL, headers=headers, json=payload)
-        print(response.json())
+    def run_analysis(self, data, existing=None):
+        '''
+        existing=None: replace None with file_name to get existing data
+        '''
+
+        if existing:
+            try:
+                with open(existing, "r") as file:
+                    return json.load(file)
+            except FileNotFoundError:
+                return {"error": f"File '{existing}' not found"}
+            except json.JSONDecodeError:
+                return {"error": f"Invalid JSON format in '{existing}'"}
+
+        else:
+            API_URL = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment-latest"
+            headers = {"Authorization": f"Bearer {self.get_api_key()}"}
+            payload = data
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
 
     
